1ce429bd1ec9695a1183fe68d5fc7a8ca0ed1ffd
Ülfet <ukacay87@gmail.com>
2022-10-12 00:39:22 +0300

    added filter

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,9 +1,22 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
+import { getCategory } from '../features/categories/categorySlice';
+import { useAppDispatch, useAppSelector } from '../utils/hooks';
+const Filter: FC = () => {
+    const dispatch = useAppDispatch();
+    const categories = useAppSelector((state) => state.categories.categories);
 
-const Filter:FC = () => {
-  return (
-    <div>Filter</div>
-  )
+    useEffect(()=>{
+        dispatch(getCategory())
+    },[]);
+    return (
+        <div className='snap-x'>
+            {React.Children.toArray(categories.map((category: any) => (
+                <div className='snap-center'>
+                    <span>{category.name}</span>
+                </div>
+            )))}
+        </div>
+    )
 }
 
 export default Filter;
