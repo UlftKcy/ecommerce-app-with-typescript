1fbd384312fecab2a74cad77025b8eec26785dc7
Ülfet <ukacay87@gmail.com>
2022-10-24 22:12:02 +0300

    added new token

--- src/api/service.ts
+++ src/api/service.ts
@@ -1,4 +1,4 @@
 export const productsBaseURL:string = 'https://upayments-studycase-api.herokuapp.com/api/products/';
 export const categoriesBaseURL:string = ' https://upayments-studycase-api.herokuapp.com/api/categories/';
-export const bearer_token:string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3QudHVyazA2QGdtYWlsLmNvbSIsImdpdGh1YiI6Imh0dHBzOi8vZ2l0aHViLmNvbS9VbGZ0S2N5IiwiaWF0IjoxNjY2MjU4MTYyLCJleHAiOjE2NjY2OTAxNjJ9.uSDp1gPzHqNgToDu1BLH-B7hFg7MDZY71g21ZLT1EM0';
+export const bearer_token:string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRlc3Q4N0BnbWFpbC5jb20iLCJnaXRodWIiOiJodHRwczovL2dpdGh1Yi5jb20vVWxmdEtjeSIsImlhdCI6MTY2NjYzODY0MywiZXhwIjoxNjY3MDcwNjQzfQ.mQu-JrdYZudITsLPrIZdHfjIgX50HiXbbARU8qMSDaA';
 
